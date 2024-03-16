3342c42df2ec337b1d6acb2b71f152750996ba5c
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-16 23:42:27 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:27 up  1:48,  1 user,  load average: 0.44, 0.35, 0.45 b147faafd688d46387a991f37623adc985f01486

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        String name = args[0];
+        try(BufferedReader br = new BufferedReader(new FileReader(name))){
+            String line;
+            while ((line=br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch (IOException error){
+            error.printStackTrace();
+        }
+    }
 }
