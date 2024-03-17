796f2032a6e25f255e30f5c2d605e9d13062f177
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-17 19:57:09 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:09 up  8:14,  1 user,  load average: 0.80, 0.64, 0.63 aa244fe2cb4524adcd160ab1dbb82df2158db7cf

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,28 @@
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main
-{    
-    public static void main(String[] args){}
+{
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.err.println("input path is required");
+        }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+        myErrorListener myListener = new myErrorListener();
+        sysYLexer.removeErrorListeners();
+        sysYLexer.addErrorListener(myListener);
+        List<? extends Token> myTokens = sysYLexer.getAllTokens();
+        if (!myListener.getError()) {
+            System.out.println(myTokens);
+        }
+
+    }
+
 }
---
+++ src/myErrorListener.java
@@ -0,0 +1,18 @@
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class myErrorListener extends BaseErrorListener {
+	boolean isErr = false;
+	public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+							int line, int charPositionInLine, String msg, RecognitionException e) {
+		isErr = true;
+		System.err.println("Error type A at Line "+ line +": "+ msg +'.');
+	}
+	public boolean getError(){
+		return isErr;
+	}
+
+
+
+}
