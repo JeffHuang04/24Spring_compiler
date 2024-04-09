b1f050f94234e2bd2e70c2cfe62b36b6c1e28ced
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-04-09 20:10:37 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:37 up 1 day,  2:40,  1 user,  load average: 0.84, 0.87, 0.72 9467ec1412296a27f12f9e93ef203dcc2f2336

--- src/myVisitor.java
+++ src/myVisitor.java
@@ -90,9 +90,9 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 				if (Objects.equals(text, "{")
 				|| Objects.equals(text, "(")
 				|| Objects.equals(text,"[")){
-					System.out.print(COLORS[colorIndex]);
 					if (Objects.equals(text, "(")
 					|| Objects.equals(text,"[")) {
+						System.out.print(COLORS[colorIndex]);
 						System.out.print(text);
 					}else {
 						LBrace(node);
@@ -221,6 +221,7 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 			&& node.getParent().getParent().getParent() instanceof SysYParser.StmtContext)){
 				//System.out.print(' ');
 				PrintSpace();
+				System.out.print(COLORS[colorIndex]);
 				System.out.print(text);//函数声明中、stmt中if、else、while代码块中的作花括号
 				System.out.println();
 			}else {
@@ -245,7 +246,7 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 	}
 
 	private void PrintSpace(){
-		System.out.println("\u001B[0m");//reset
+		System.out.print("\u001B[0m");//reset
 		System.out.print(' ');
 		System.out.print(colorTemp);
 		if (!Objects.equals(underLineTemp, "")){
