5f3a3298dce55155dad06e3a9692fef777b57fd6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-04 12:59:18 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:59:18 up 19:41,  1 user,  load average: 0.87, 0.62, 0.62 c39328313c2a3093c9f3c8637a5c01de40984717

--- src/myVisitor.java
+++ src/myVisitor.java
@@ -143,10 +143,11 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 		} else if (symbolTableStack.findAll(LValName) instanceof IntType && !ctx.L_BRACKT().isEmpty()) {
 			outputHelper.outputErr(ErrorType.NOT_AN_ARRAY.getCode(), ctx.IDENT().getSymbol().getLine(),ErrorType.NOT_AN_ARRAY.getMessage());
 			return null;
-		} else if (symbolTableStack.findAll(LValName) instanceof FunctionType) {
-			outputHelper.outputErr(ErrorType.ILLEGAL_ASSIGNMENT_TARGET.getCode(),ctx.IDENT().getSymbol().getLine(),ErrorType.ILLEGAL_ASSIGNMENT_TARGET.getMessage());
-			return null;
 		}
+//		else if (symbolTableStack.findAll(LValName) instanceof FunctionType) {
+//			outputHelper.outputErr(ErrorType.ILLEGAL_ASSIGNMENT_TARGET.getCode(),ctx.IDENT().getSymbol().getLine(),ErrorType.ILLEGAL_ASSIGNMENT_TARGET.getMessage());
+//			return null;
+//		}
 
 		return null;
 	}
