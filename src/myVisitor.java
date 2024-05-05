921f55a3dd0698d0ce813a1f86e84039c34670e5
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-05 11:21:17 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:17 up 1 day,  8:52,  1 user,  load average: 1.01, 0.86, 0.75 f91efddb2bdc4debb6950b52f785faafa3c16277

--- src/myVisitor.java
+++ src/myVisitor.java
@@ -228,10 +228,10 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 
 
 			}
-//			else if (!(funcFParamsExp.isEmpty()&&funcRParamsAct.isEmpty())){
-//				outputHelper.outputErr(ErrorType.FUNCTION_NOT_APPLICABLE.getCode(), ctx.funcRParams().getStart().getLine(),ErrorType.FUNCTION_NOT_APPLICABLE.getMessage());
-//				return null;
-//			}
+			else if (!(funcFParamsExp.isEmpty()&&funcRParamsAct.isEmpty())){
+				outputHelper.outputErr(ErrorType.FUNCTION_NOT_APPLICABLE.getCode(), ctx.funcRParams().getStart().getLine(),ErrorType.FUNCTION_NOT_APPLICABLE.getMessage());
+				return null;
+			}
 		}
 		return null;
 	}
