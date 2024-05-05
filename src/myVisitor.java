d1c293dfc4bc8dd10aaf8faa32abce3c7fcfb4ef
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-05 20:06:56 +0800

    > make 221250148 ubuntu Linux ubuntu-legion 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:56 up 15 min,  1 user,  load average: 0.81, 0.97, 0.74 65ba636a5c94a603e63cec2bf4b70bbd7ff47b76

--- src/myVisitor.java
+++ src/myVisitor.java
@@ -225,15 +225,10 @@ public class myVisitor extends SysYParserBaseVisitor<Void>{
 					outputHelper.outputErr(ErrorType.FUNCTION_NOT_APPLICABLE.getCode(), ctx.funcRParams().getStart().getLine(),ErrorType.FUNCTION_NOT_APPLICABLE.getMessage());
 					return null;
 				}//数量不一样 报错
-			}
-			boolean is = !(funcFParamsExp.isEmpty()&&funcRParamsAct.isEmpty());
-			if (is){
-				int a = 1;
-			}
-//			else if (!(funcFParamsExp.isEmpty()&&funcRParamsAct.isEmpty())){
-//				outputHelper.outputErr(ErrorType.FUNCTION_NOT_APPLICABLE.getCode(), ctx.funcRParams().getStart().getLine(),ErrorType.FUNCTION_NOT_APPLICABLE.getMessage());
-//				return null;
-//			}
+			} else if (!(funcFParamsExp.isEmpty()&&funcRParamsAct.isEmpty())){
+				outputHelper.outputErr(ErrorType.FUNCTION_NOT_APPLICABLE.getCode(), ctx.getStart().getLine(),ErrorType.FUNCTION_NOT_APPLICABLE.getMessage());
+				return null;
+			}//funcRParams为空，触发空指针了
 
 
 
